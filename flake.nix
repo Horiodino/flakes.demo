840bfc1a54cd93f1d78ca6cfa3454c71ba899f56
horiodino <holiodin@gmail.com>
2024-05-11 16:27:36 +0530

    dontUnpack=true
    Signed-off-by: horiodino <holiodin@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,34 +1,36 @@
 {
-  description = "kbuilder ";  
+  description = "kubebuilder";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs = {self, nixpkgs}: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-
-      stdenv.mkDerivation rec {
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation rec {
         name = "kubebuilder";
         version = "3.14.2";
 
-        src = fetchurl {
+        src = pkgs.fetchurl {
           url = "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.14.2/kubebuilder_linux_amd64";
           sha256 = "sha256-RCTN6C2PUjyiAN2Uy+HTUUQRsHOQRuQevFOGLqGZyQQ=";
         };
 
-        sourceRoot = ".";
-
+        dontUnpack = true;
 
         installPhase = ''
-        install -m775 -D kubebuilder $out/bin/kubebuilder
+          install -m775 -D $src $out/bin/kubebuilder
         '';
 
         shellHook = ''
-        echo "Kubebuilder is available: $out/bin/kubebuilder"
-        $out/bin/kubebuilder version
-      '';
+          echo $src
+          echo "Kubebuilder is available: $out/bin/kubebuilder"
+          $out/bin/kubebuilder version
+        '';
       };
-  };
+    };
 }
+
