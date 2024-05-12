b403beb4074a3c99b14133e86bf3b4ae0186cf26
horiodino <holiodin@gmail.com>
2024-05-12 17:38:15 +0530

    fixed
    Signed-off-by: horiodino <holiodin@gmail.com>

--- flake.nix
+++ flake.nix
@@ -7,34 +7,39 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation rec {
-        name = "kubebuilder";
-        version = "3.14.2";
+      packages = forEachSystem ({ pkgs, ... }: {
+        default = pkgs.stdenv.mkDerivation rec {
+          name = "kubebuilder";
+          version = "3.14.2";
 
-        src = pkgs.fetchurl {
-          url = "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.14.2/kubebuilder_linux_amd64";
-          sha256 = "sha256-RCTN6C2PUjyiAN2Uy+HTUUQRsHOQRuQevFOGLqGZyQQ=";
+          src = pkgs.fetchurl {
+            url = "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v3.14.2/kubebuilder_linux_amd64";
+            sha256 = "sha256-RCTN6C2PUjyiAN2Uy+HTUUQRsHOQRuQevFOGLqGZyQQ=";
+          };
+
+          dontUnpack = true;
+          phases = [ "installPhase" ];
+          dontBuild = true;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/kubebuilder
+            chmod +x $out/bin/kubebuilder
+          '';
+
+          shellHook = ''
+            echo $src
+            echo "Kubebuilder is available: $out/bin/kubebuilder"
+            $out/bin/kubebuilder version
+          '';
         };
-
-        dontUnpack = true;
-	phases = [ "installPhase" ];
-	dontBuild = true;
-
-        installPhase = ''
-	  mkdir -p $out/bin
-          cp $src $out/bin/kubebuilder
-	  chmod +x $out/bin/kubebuilder
-        '';
-
-        shellHook = ''
-          echo $src
-          echo "Kubebuilder is available: $out/bin/kubebuilder"
-          $out/bin/kubebuilder version
-        '';
-      };
+      });
     };
 }
-
